62797248bcf7031e1e6eca51e122e712a988b029
Muneera <muneeraalmuhareb@gmail.com>
2026-05-11 11:50:09 +0000

    fix duplicate resource creation with lifecycle protection

--- secrets.tf
+++ secrets.tf
@@ -1,12 +1,8 @@
-resource "google_secret_manager_secret" "db_password" {
-  secret_id = "db-password"
-
-  replication {
-    auto {}
-  }
+resource "google_compute_network" "secure_vpc" {
+  name                    = "secure-vpc"
+  auto_create_subnetworks = false
 }
 
-resource "google_secret_manager_secret_version" "db_password_version" {
-  secret      = google_secret_manager_secret.db_password.id
-  secret_data = "MySecurePassword123!"
+lifecycle {
+  prevent_destroy = true
 }
